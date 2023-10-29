b3040bdc01811f3ce8a54a03abef8f3c4baaabb5
Josip Igrec <josip@igr.ec>
2023-10-29 21:13:18 +0100

    Add some util types

--- src/lib/util/class.ts
+++ src/lib/util/class.ts
@@ -1,2 +1,5 @@
-export const cn = (...classes: (string | null | undefined)[]) =>
+import { type Falsy } from "~/types/util";
+
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+export const cn = (...classes: (string | Falsy)[]) =>
   classes.filter(Boolean).join(" ");
