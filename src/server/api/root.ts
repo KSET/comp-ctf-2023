e175ee71af9f20f3884df10fd7280ec653c3fdef
Josip Igrec <josip@igr.ec>
2023-10-29 21:23:57 +0100

    Remove placeholder post router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,7 +2,6 @@ import { createTRPCRouter } from "~/server/api/trpc";
 
 import { authRouter } from "./routers/auth";
 import { pageDataRouter } from "./routers/pageData";
-import { postRouter } from "./routers/post";
 
 /**
  * This is the primary router for your server.
@@ -10,7 +9,6 @@ import { postRouter } from "./routers/post";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   auth: authRouter,
   pageData: pageDataRouter,
 });
